3e4ddc4c53f9bfd49f5becf2fb0eadaa0265f89a
rizkiarch <muha20098ti@student.nurulfikri.ac.id>
2024-09-29 02:54:18 +0700

    table category

--- src/pages/category/TableCategories.tsx
+++ src/pages/category/TableCategories.tsx
@@ -1,4 +1,3 @@
-import axios from "axios"
 import React, { useEffect, useState } from "react"
 import {
   Table,
@@ -7,9 +6,17 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  getKeyValue,
   Spinner,
+  Button,
+  Pagination,
+  SortDescriptor,
+  Input,
+  Tooltip,
 } from "@nextui-org/react"
+import { SearchIcon } from "../../components/SearchIcon"
+import { PlusIcon } from "../../components/PlusIcon"
+import { EditIcon } from "../../components/EditIcon"
+import { DeleteIcon } from "../../components/DeleteIcon"
 
 type CategoryType = {
   id: number
@@ -19,16 +26,19 @@ type CategoryType = {
 export default function TableCategories() {
   const [categories, setCategories] = useState<CategoryType[]>([])
   const [isLoading, setIsLoading] = React.useState(true)
+  const [filterValue, setFilterValue] = React.useState("")
   const [page, setPage] = React.useState(1)
-
-  //   const [rowsPerPage, setRowsPerPage] = React.useState(5)
-
+  const [rowsPerPage, setRowsPerPage] = React.useState(5)
+  const hasSearchFilter = Boolean(filterValue)
+  const [sortDescriptor, setSortDescriptor] = React.useState<SortDescriptor>({
+    column: "name",
+    direction: "ascending",
+  })
   async function getCategories() {
     const response = await fetch("/api/categories")
     const data = await response.json()
     setIsLoading(false)
 
-    console.log(data.categories)
     if (response.ok) {
       setCategories(data.categories)
     }
@@ -38,49 +48,7 @@ export default function TableCategories() {
     getCategories()
   }, [])
 
-  let list = useAsyncList<CategoryType>({
-    async load({ signal, cursor }) {
-      if (cursor) {
-        setPage((prev) => prev + 1)
-      }
-
-      const res = await fetch(
-        cursor || "https://swapi.py4e.com/api/people/?search=",
-        { signal }
-      )
-      let json = await res.json()
-
-      if (!cursor) {
-        setIsLoading(false)
-      }
-
-      return {
-        items: json.results,
-        cursor: json.next,
-      }
-    },
-  })
-
-  //   const getCategories = async () => {
-  //     try {
-  //       const response = await axios.get(`api/categories`)
-  //       setCategories(response.data?.data)
-
-  //       console.log(response.data)
-  //     } catch (error) {
-  //       console.error(error)
-  //     }
-  //   }
-
-  //   useEffect(() => {
-  //     getCategories()
-  //   }, [])
-
   const columns = [
-    {
-      key: "no",
-      label: "#",
-    },
     {
       key: "name",
       label: "NAME",
@@ -91,30 +59,195 @@ export default function TableCategories() {
     },
   ]
 
-  const hasMore = page < 9
+  const renderCell = React.useCallback(
+    (category: CategoryType, columnKey: React.Key, index: number) => {
+      const cellValue = category[columnKey as keyof CategoryType]
+
+      switch (columnKey) {
+        case "name":
+          return <p className="text-bold text-small capitalize">{cellValue}</p>
+        case "actions":
+          return (
+            <div className="relative flex items-center gap-2">
+              <Tooltip content="Edit user">
+                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                  <EditIcon />
+                </span>
+              </Tooltip>
+              <Tooltip color="danger" content="Delete user">
+                <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                  <DeleteIcon />
+                </span>
+              </Tooltip>
+            </div>
+          )
+        default:
+          return <span>{cellValue}</span>
+      }
+    },
+    [page, rowsPerPage]
+  )
+
+  const filteredItems = React.useMemo(() => {
+    let filterData = [...categories]
+
+    if (hasSearchFilter) {
+      filterData = filterData.filter((category) =>
+        category.name.toLowerCase().includes(filterValue.toLowerCase())
+      )
+    }
+
+    return filterData
+  }, [categories, filterValue, hasSearchFilter])
+
+  console.log(filteredItems)
+
+  const pages = Math.ceil(filteredItems.length / rowsPerPage)
+
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage
+    const end = start + rowsPerPage
+
+    return filteredItems.slice(start, end)
+  }, [page, filteredItems, rowsPerPage])
+
+  const sortedItems = React.useMemo(() => {
+    return [...items].sort((a: CategoryType, b: CategoryType) => {
+      const first = a[sortDescriptor.column as keyof CategoryType] as number
+      const second = b[sortDescriptor.column as keyof CategoryType] as number
+      const cmp = first < second ? -1 : first > second ? 1 : 0
+
+      return sortDescriptor.direction === "descending" ? -cmp : cmp
+    })
+  }, [sortDescriptor, items])
+
+  const onRowsPerPageChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setRowsPerPage(Number(e.target.value))
+      setPage(1)
+    },
+    []
+  )
+
+  const onSearchChange = React.useCallback((value?: string) => {
+    if (value) {
+      setFilterValue(value)
+      setPage(1)
+    } else {
+      setFilterValue("")
+    }
+  }, [])
+
+  const onClear = React.useCallback(() => {
+    setFilterValue("")
+    setPage(1)
+  }, [])
+
+  const topContent = React.useMemo(() => {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between gap-3 items-end">
+          <Input
+            isClearable
+            className="w-full sm:max-w-[44%]"
+            placeholder="Search by name..."
+            startContent={<SearchIcon />}
+            value={filterValue}
+            onClear={() => onClear()}
+            onValueChange={onSearchChange}
+          />
+          <div className="flex gap-3">
+            <Button color="primary" endContent={<PlusIcon />}>
+              Add New
+            </Button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-default-400 text-small">
+            Total {categories.length} categories
+          </span>
+          <label className="flex items-center text-default-400 text-small">
+            Rows per page:
+            <select
+              className="bg-transparent outline-none text-default-400 text-small"
+              onChange={onRowsPerPageChange}
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </div>
+      </div>
+    )
+  }, [
+    filterValue,
+    onSearchChange,
+    onRowsPerPageChange,
+    categories.length,
+    hasSearchFilter,
+  ])
+
+  const onNextPage: () => void = React.useCallback(() => {
+    if (page < pages) {
+      setPage(page + 1)
+    }
+  }, [page, pages])
+
+  const onPreviousPage = React.useCallback(() => {
+    if (page > 1) {
+      setPage(page - 1)
+    }
+  }, [page])
+
+  const bottomContent = React.useMemo(() => {
+    return (
+      <div className="py-2 px-2 flex justify-between items-center">
+        <span className="w-[30%] text-small text-default-400"></span>
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color="primary"
+          page={page}
+          total={pages}
+          onChange={setPage}
+        />
+        <div className="hidden sm:flex w-[30%] justify-end gap-2">
+          <Button
+            isDisabled={pages === 1}
+            size="sm"
+            variant="flat"
+            onPress={onPreviousPage}
+          >
+            Previous
+          </Button>
+          <Button
+            isDisabled={pages === 1}
+            size="sm"
+            variant="flat"
+            onPress={onNextPage}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    )
+  }, [page, pages])
 
   return (
     <>
       <Table
-        isHeaderSticky
         aria-label="Example table with client side sorting"
-        bottomContent={
-          hasMore && !isLoading ? (
-            <div className="flex w-full justify-center">
-              <Button
-                isDisabled={list.isLoading}
-                variant="flat"
-                onPress={list.loadMore}
-              >
-                {list.isLoading && <Spinner color="white" size="sm" />}
-                Load More
-              </Button>
-            </div>
-          ) : null
-        }
+        isHeaderSticky
+        topContent={topContent}
+        topContentPlacement="outside"
+        bottomContent={bottomContent}
+        bottomContentPlacement="outside"
+        sortDescriptor={sortDescriptor}
+        onSortChange={setSortDescriptor}
         classNames={{
-          base: "max-h-[520px] overflow-scroll",
-          table: "min-h-[420px]",
+          wrapper: "max-h-[382px]",
         }}
       >
         <TableHeader columns={columns}>
@@ -126,12 +259,12 @@ export default function TableCategories() {
           isLoading={isLoading}
           loadingContent={<Spinner label="Loading..." />}
           emptyContent={"No rows to display."}
-          items={categories}
+          items={sortedItems}
         >
           {(item) => (
             <TableRow key={item.id}>
               {(columnKey) => (
-                <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+                <TableCell>{renderCell(item, columnKey)}</TableCell>
               )}
             </TableRow>
           )}
